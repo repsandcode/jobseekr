f8ae5ffab522da14c1ed030d98f6c53ab4b0ca9c
repsandcode <albertjonathan.dev@gmail.com>
2023-08-22 11:16:33 +0800

    change api server url

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4400/api-v1";
+// const API_URL = "http://localhost:4400/api-v1";
+const API_URL = "https://jobseekr.onrender.com/api-v1/";
 
 export const API = axios.create({
   baseURL: API_URL,
